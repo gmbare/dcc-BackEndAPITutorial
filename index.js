e7b89f5cb60cf5eb4044b02cf28c121b31156d35
Garett Bare <Garett.Bare@yahoo.com>
2022-03-31 21:43:38 -0500

    tweaking the failure to update return and added CORS

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 //Middleware
 app.use(express.json())
-app.use(express.urlencoded)
+app.use(express.urlencoded({extended:true}));
 
 //Endpoints
 //http://localhost:5005/api/products
@@ -24,9 +24,22 @@ app.get("/api/products/:id", (req, res) => {
 });
 
 app.post("/api/products", (req, res) => {
-    const id = req.params.id;
-    const products = repoContext.products.findProductById(id);
-    return(res.send(products))
+    const newProduct = req.body;
+    const addedProduct = repoContext.products.createProduct(newProduct);
+    return (res.status(201).send(addedProduct))
+});
+
+app.put("/api/products/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const productPropertiesToModify = req.body;
+    const updateProduct = repoContext.products.updateProduct(id, productPropertiesToModify);
+    return (res.status(201).send(updateProduct))
+});
+
+app.delete("/api/products/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const deleteProduct = repoContext.products.deleteProduct(id);
+    return (res.status(201).send(deleteProduct))
 });
 
 //starting a server
